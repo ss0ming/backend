45f900136d4412cfba980cdcf4a3c53560580865
ss0ming <hee9920@naver.com>
2025-03-19 01:03:20 +0900

    feat: add Delivery Manager get single

--- deliverymangerservice/delivery-manager-service/src/main/java/on/logistics/deliverymanagerservice/presentation/DeliveryManagerController.java
+++ deliverymangerservice/delivery-manager-service/src/main/java/on/logistics/deliverymanagerservice/presentation/DeliveryManagerController.java
@@ -1,13 +1,17 @@
 package on.logistics.deliverymanagerservice.presentation;
 
 import jakarta.validation.Valid;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import on.logistics.deliverymanagerservice.application.dtos.CreateDeliveryManagerRequestDto;
 import on.logistics.deliverymanagerservice.application.service.DeliveryManagerService;
 import on.logistics.deliverymanagerservice.global.presentation.dtos.CommonResponse;
 import on.logistics.deliverymanagerservice.presentation.dtos.request.CreateDeliveryManagerRequest;
 import on.logistics.deliverymanagerservice.presentation.dtos.response.CreateDeliveryManagerResponse;
+import on.logistics.deliverymanagerservice.presentation.dtos.response.GetDeliveryManagerResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +23,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeliveryManagerController {
 
     private final DeliveryManagerService deliveryManagerService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CommonResponse<GetDeliveryManagerResponse>> getDeliveryManager(
+        @PathVariable UUID id) {
+        final var responseDto = deliveryManagerService.getDeliveryManager(id);
+        return ResponseEntity.ok(CommonResponse.success(responseDto));
+    }
 
     @PostMapping
     public ResponseEntity<CommonResponse<CreateDeliveryManagerResponse>> createDeliveryManager(
